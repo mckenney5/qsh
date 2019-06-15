e3c101c5623da0155803a335e5be800722a6ab05
mckenney5 <mckenneyadam@gmail.com>
2019-06-14 20:50:51 -0400

    Added comments and fixed fn to get current dir name

--- src/libs/linux.h
+++ src/libs/linux.h
@@ -11,6 +11,8 @@
 #include <sys/wait.h>	//run()
 #include <sys/stat.h>	//isFile();
 
+#define _MAX_DIR_NAME 210
+
 /* Global READ ONLY vars */
 const char DEFAULT_EXEC_PATH1[] = "/bin/";
 const char DEFAULT_EXEC_PATH2[] = "/usr/bin/";
@@ -19,24 +21,29 @@ const char DEFAULT_EXEC_PATH3[] = "/sbin";
 static const char HOME[] = "/home/";
 
 int get_hostname(char *hostname, size_t len){
+/* Used to get the computers name */
 	return gethostname(hostname, len);
 }
 
 char *get_cwd(char *buf, size_t size){
+/* Used to get the current working dir */
 	return getcwd(buf, size);
 }
 
 void get_cwd_last(char* buf, size_t size){
+/* Used to get the name of the dir we are in */
 	get_cwd(buf, size);
 	char *last;
 	last = strrchr(buf, '/');
+	char temp[_MAX_DIR_NAME] = {'\0'};
+	strncpy(temp, last, _MAX_DIR_NAME);
 	if(last == NULL)
 		perror("Error in program, NULL pointer in get_cwd_last");
-	else if(!strcmp("/", last))
-		strncpy(buf, last, size);
+	else if(!strcmp("/", temp))
+		strncpy(buf, temp, size);
 	else {	
 		//copy all but the '/'
-		strncpy(buf, last, size);
+		strncpy(buf, temp, size);
 		unsigned int i;
 		for(i=0; buf[i] != '\0'; i++)
 			buf[i] = buf[i+1];
@@ -44,12 +51,14 @@ void get_cwd_last(char* buf, size_t size){
 }
 
 void get_user(char *buf, size_t size){
+/* Used to get username */
 	struct passwd *pass;
 	pass = getpwuid(getuid());
 	strncpy(buf, pass->pw_name, size);
 }
 
 void get_home(char *buf, size_t size){
+/* Used to get the user's home dir location */
 	char user[255];
 	get_user(user, sizeof(user));
 	snprintf(buf, size, "%s%s", HOME, user); 
